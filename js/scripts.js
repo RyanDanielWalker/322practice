c10dbe0e779b02f8a11ec2ccf107b39c33df5580
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-22 15:02:21 -0700

    Add jQuery information and click functions
    Co-authored-by: Michael Burton <micbur1@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
-function cToF (){
-  const fahrenheit = parseInt((celsius * 2 + 30));
-  alert(fahrenheit + " °F");
-}
+jQuery("h1").click(function() {
+  alert("This is a header.");
+  alert("I told you, THIS IS A HEADER!");
+});
 
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
 
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
 
-const celsius = prompt("enter a celsius degree");
-// const fahrenheit = parseInt((celsius * 2 + 30));
-// alert(fahrenheit);
-cToF();
+jQuery("body").click(function(){
+  alert("This is the body.");
+});
+
